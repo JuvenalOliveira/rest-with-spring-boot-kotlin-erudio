f9ea076b57998728f9b421502e513908463f33bf
ryanjuvenal <ryansantosoliveira100@gmail.com>
2024-06-25 16:05:59 -0300

    EDEN-1350 Adicionando arquivo .properties e adicionando conexão com o postgres

--- src/main/kotlin/br/com/erudio/repository/PersonRepository.kt
+++ src/main/kotlin/br/com/erudio/repository/PersonRepository.kt
@@ -2,7 +2,7 @@ package br.com.erudio.repository
 
 import br.com.erudio.model.Person
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+//import org.springframework.stereotype.Repository
 
-@Repository
+//@Repository
 interface PersonRepository : JpaRepository<Person, Long?>
